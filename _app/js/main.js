9389e42f58c4aa2a7f2e565450446646c6b971ee
olekristian1993 <olekristianolaisen@gmail.com>
2023-05-04 14:40:40 +0200

    added remove functionality to favorite button

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,8 +1,9 @@
 
 import Listings from "./modules/listings.js";
 import Favorites from "./modules/favorites.js";
+import Item from "./modules/item.js";
 
 
 Listings();
-
 Favorites();
+Item();
--- _app/js/modules/listings.js
+++ _app/js/modules/listings.js
@@ -11,6 +11,7 @@ export default async function Listings() {
   async function fetchListings() {
     
     const query = `*[_type == 'listing'] {
+      _id,
       'image': image.asset->url,
       'title': name,
       'price': price,
@@ -22,9 +23,9 @@ export default async function Listings() {
   function createListingContainerDOM() {
     const gridContainer = document.createElement('div');
     gridContainer.className = 'main__listings-grid-container';
-
+    
     const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-
+    
     for (const listing of listings) {
       const gridItem = document.createElement('div');
       const listingDetails = document.createElement('div');
@@ -33,7 +34,7 @@ export default async function Listings() {
       const listingPrice = document.createElement('p');
       const listingCity = document.createElement('p');
       const favoriteButton = document.createElement('button');
-
+      
       gridItem.className = 'main__listings-grid-item';
       listingDetails.className = 'main__listings-grid-item-details';
       listingTitle.className = 'main__listings-grid-item-title';
@@ -41,29 +42,30 @@ export default async function Listings() {
       listingPrice.className = 'main__listings-grid-item-price';
       listingCity.className = 'main__listings-grid-item-city';
       favoriteButton.className = 'main__listings-grid-item-favorite bi bi-heart';
-
+      
+      gridItem.href = 'item.html?id=' + listing._id;
       listingTitle.innerText = listing.title;
       listingImage.src = listing.image;
       listingPrice.innerText = `${listing.price.number} ${listing.price.currency}`;
       listingCity.innerText = listing.city;
-
+      
       if (favorites.some((favorite) => favorite.title === listing.title)) {
         favoriteButton.className = 'main__listings-grid-item-favorite bi bi-heart-fill';
       }
       else {
         favoriteButton.className = 'main__listings-grid-item-favorite bi bi-heart';
       }
-
+      
       gridItem.appendChild(listingImage);
       gridItem.appendChild(listingDetails);
       gridItem.appendChild(favoriteButton);
-
+      
       listingDetails.appendChild(listingCity);
       listingDetails.appendChild(listingTitle);
       listingDetails.appendChild(listingPrice);
-
+      
       gridContainer.appendChild(gridItem);
-
+      
       favoriteButton.addEventListener('click', () => {
         if (favoriteButton.classList.contains('bi-heart-fill')) {
           favoriteButton.classList.remove('bi-heart-fill');
@@ -76,16 +78,26 @@ export default async function Listings() {
         }
       });
     }
-
+    
     return gridContainer;
   }
-
+  
   
   function addFavorite(listing) {
     const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
     favorites.push(listing);
     localStorage.setItem('favorites', JSON.stringify(favorites));
     renderFavorites();
+  }
+  
+  function removeFavorite(listing) {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const index = favorites.findIndex((favorite) => favorite.title === listing.title);
+    if (index > -1) {
+      favorites.splice(index, 1);
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+      renderFavorites();
+    }
   }
 
   
@@ -95,7 +107,7 @@ export default async function Listings() {
     listingList.innerHTML = '';
     listingList.appendChild(listContainer);
   }
-
+  
   
   
   await fetchListings();
