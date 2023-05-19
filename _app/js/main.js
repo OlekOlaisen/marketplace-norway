e61e8261d5c8fed251e6a54d2c33b04c043bc4f9
Ole Kristian Heian Olaisen <olekristianolaisen@gmail.com>
2023-05-19 20:53:06 +0200

    Feature: Properties

--- _app/js/main.js
+++ _app/js/main.js
@@ -3,6 +3,10 @@ import Listings from "./modules/listings.js";
 import Favorites from "./modules/favorites.js";
 import Item from "./modules/item.js";
 import Jobs from "./modules/jobs.js";
+import jobDetails from "./modules/jobDetails.js";
+import Properties from "./modules/properties.js";
+
+
 
 
 
@@ -10,3 +14,5 @@ Listings();
 Favorites();
 Item();
 Jobs();
+jobDetails();
+Properties();
