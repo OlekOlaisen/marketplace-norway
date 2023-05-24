22d504eec2ef8dfa0e813125e9b8f3b30c8ac059
Ole Kristian Heian Olaisen <olekristianolaisen@gmail.com>
2023-05-24 16:14:38 +0200

    Feature: Created responsive header

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,22 +1,20 @@
-
-import Listings from "./modules/listings.js";
+import Header from "./modules/header.js";
 import Favorites from "./modules/favorites.js";
+import Listings from "./modules/listings.js";
 import itemDetails from "./modules/itemDetails.js";
 import Jobs from "./modules/jobs.js";
 import jobDetails from "./modules/jobDetails.js";
 import Properties from "./modules/properties.js";
-import Header from "./modules/header.js";
 import propertyDetails from "./modules/propertyDetails.js";
 
 
 
 
-
+Header();
 Listings();
 Favorites();
 itemDetails();
 Jobs();
 jobDetails();
 Properties();
-Header();
 propertyDetails();
--- _app/js/modules/header.js
+++ _app/js/modules/header.js
@@ -1,4 +1,28 @@
 export default async function Header() {
-   console.log("test");
 
+   const toggleButton = document.querySelector('.header__top-toggle');
+   const bottomHeader = document.querySelector('.header__bottom');
+
+   function handleResize() {
+      if (window.innerWidth <= 768) {
+         if (!bottomHeader.classList.contains('hidden')) {
+            bottomHeader.classList.add('hidden');
+         }
+      } else {
+         bottomHeader.classList.remove('hidden');
+         bottomHeader.classList.remove('visible');
+      }
+   }
+
+   window.addEventListener('resize', handleResize);
+   handleResize(); 
+
+   toggleButton.addEventListener('click', handleToggleButtonClick);
+
+   function handleToggleButtonClick() {
+      if (window.innerWidth <= 768) {
+         bottomHeader.classList.toggle('hidden');
+         bottomHeader.classList.toggle('visible');
+      }
+   }
 }
